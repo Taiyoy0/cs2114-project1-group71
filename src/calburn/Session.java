12253cd82ce03c392d8da681f32d84222764a740
Taiyoy0 <taiyoyo07@gmail.com>
2026-09-19 11:54:26 -0400

    Fixed build path stuff, completed Session and test class for Session

---
+++ src/calburn/Session.java
@@ -0,0 +1,119 @@
+package calburn;
+
+import java.time.Instant;
+
+// -------------------------------------------------------------------------
+/**
+ * A single logged session of an exercise
+ * 
+ * @author taiyol
+ * @version Sep 16, 2026
+ */
+public class Session {
+    // ~ Fields ................................................................
+    private Exercise exercise;
+    private int durationMin;
+    private Instant time;
+    private double caloriesBurned;
+
+    // ~ Constructors ..........................................................
+
+    /**
+     * Constructs a Session object. Calculates caloriesBurned from given values.
+     * 
+     * @param exercise
+     *            The type of exercise
+     * @param durationMin
+     *            the duration the exercise was done for
+     * @param weightKg
+     *            the weight of the individual during the activity
+     * @param time
+     *            the time the exercise was done
+     * 
+     * @throws IllegalArgumentException
+     *             if the duration is <= 0 or > 1440 minutes
+     */
+    public Session(
+        Exercise exercise,
+        int durationMin,
+        double weightKg,
+        Instant time)
+        throws IllegalArgumentException {
+        if (durationMin <= 0) {
+            throw new IllegalArgumentException(
+                "Duration cannot be equal to or less than 0 minutes");
+        }
+        if (durationMin > 1440) {
+            throw new IllegalArgumentException(
+                "Duration cannot be more than 24 houurs (1440 minutes)");
+        }
+
+        this.exercise = exercise;
+        this.durationMin = durationMin;
+        this.time = time;
+
+        caloriesBurned = (exercise.getMETValue() * 3.5 * weightKg / 200)
+            * durationMin;
+    }
+
+
+    /**
+     * Constructs a Session object. Calculates caloriesBurned from given values.
+     * defaults time to Instant.now()
+     * 
+     * @param exercise
+     *            The type of exercise
+     * @param durationMin
+     *            the duration the exercise was done for
+     * @param weightKg
+     *            the weight of the individual during the activity
+     * 
+     * @throws IllegalArgumentException
+     *             if the duration is <= 0 or > 1440 minutes
+     */
+    public Session(Exercise exercise, int durationMin, double weightKg)
+        throws IllegalArgumentException {
+        this(exercise, durationMin, weightKg, Instant.now());
+    }
+
+
+    /**
+     * Returns the exercise name
+     * 
+     * @return The exercise done
+     */
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+
+    /**
+     * Returns the duration of the session in minutes
+     * 
+     * @return the duration of exercise
+     */
+    public int getDuration() {
+        return durationMin;
+    }
+
+
+    /**
+     * Returns the time the exercise was completed
+     * 
+     * @return the time in ISO-8601 format, UTC
+     */
+    public Instant getDate() {
+        return time;
+    }
+
+
+    /**
+     * Returns the calculated number of calories burned during the session
+     * 
+     * @return theoretical calories burned
+     */
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+}
---
+++ src/calburn/SessionTest.java
@@ -0,0 +1,104 @@
+package calburn;
+
+import java.time.Instant;
+
+// -------------------------------------------------------------------------
+/**
+ * Test class for Session
+ * 
+ * @author taiyol
+ * @version Sep 19, 2026
+ */
+public class SessionTest extends student.TestCase {
+
+    private Exercise running;
+    private Session session1;
+    private Session session2;
+
+    /**
+     * Set up for testing Session class
+     */
+    public void setUp() {
+        running = new Exercise("Running", 12.5);
+        session1 = new Session(running, 15, 60, Instant.parse(
+            "2026-09-19T15:30:00Z"));
+        session2 = new Session(running, 15, 60);
+    }
+
+
+    /**
+     * Tests Session constructor and it's exception throwing
+     */
+    public void testSession() {
+        Exception exception = new Exception();
+        try {
+            session1 = new Session(running, 0, 60, Instant.now());
+        }
+        catch (IllegalArgumentException e) {
+            exception = e;
+        }
+
+        assertNotNull(exception);
+        assertEquals("Duration cannot be equal to or less than 0 minutes",
+            exception.getMessage());
+
+        exception = null;
+        try {
+            session1 = new Session(running, -5, 60, Instant.now());
+        }
+        catch (IllegalArgumentException e) {
+            exception = e;
+        }
+
+        assertNotNull(exception);
+        assertEquals("Duration cannot be equal to or less than 0 minutes",
+            exception.getMessage());
+
+        exception = null;
+        try {
+            session1 = new Session(running, 1441, 60, Instant.now());
+        }
+        catch (IllegalArgumentException e) {
+            exception = e;
+        }
+
+        assertNotNull(exception);
+        assertEquals("Duration cannot be more than 24 houurs (1440 minutes)",
+            exception.getMessage());
+    }
+
+
+    /**
+     * Tests getExercise method
+     */
+    public void testGetExercise() {
+        assertEquals(running, session1.getExercise());
+    }
+
+
+    /**
+     * Tests getDuration method
+     */
+    public void testGetDuration() {
+        assertEquals(15, session1.getDuration());
+    }
+
+
+    /**
+     * Tests getDate method
+     */
+    public void testGetDate() {
+        assertTrue(Instant.parse("2026-09-19T15:30:00Z").equals(session1
+            .getDate()));
+
+        assertNotNull(session2.getDate());
+    }
+
+
+    /**
+     * Tests getCaloriesBurned method
+     */
+    public void testGetCaloriesBurned() {
+        assertEquals(196.875, session1.getCaloriesBurned(), 0.001);
+    }
+}
